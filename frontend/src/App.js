c022a664deba7dc581a2517e8fb658a08a10509b
Pixelynx <tjstevens1992@gmail.com>
2019-02-12 16:53:15 -0500

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { NavBar } from "./components/navigation/navBar.jsx";
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,9 @@ class App extends Component {
 
   render() {
     return(
-      <div>hi</div>
+      <>
+        <NavBar />
+      </>
     )
   }
 }
