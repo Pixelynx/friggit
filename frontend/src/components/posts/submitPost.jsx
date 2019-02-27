eaaa58b2dc7f4a1e8243a5563b0a63699626ed30
pixelynx <tjstevens1992@gmail.com>
2019-02-27 16:38:16 -0500

    fixes

--- frontend/src/components/posts/submitPost.jsx
+++ frontend/src/components/posts/submitPost.jsx
@@ -15,45 +15,47 @@ class SubmitPost extends Component {
   render() {
     return(
       <>
-          <div className='submit_post_container'>
-            <span className='create_post_text'>Create a post</span>
-            <br />
-            <select className='choose_community_dropdown'>
-              <option>Choose a community</option>
-              <option>Prolly Won't be a Select Element</option>
-              <option>Prolly Won't be a Select Element</option>
-              <option>Prolly Won't be a Select Element</option>
-              <option>Prolly Won't be a Select Element</option>
-            </select>
-            <br />
-
-          <div className='post_box'>
-
-            <div className='post_options'>
-              <div id='post'>Post</div>
-              <div id='img_vid'>Image & Video</div>
-              <div id='link'>Link</div>
-            </div>
-
-            <input type='text' className='title_input' placeholder='Title' />
+        <div className='submit_post_containers_container'>
+            <div className='submit_post_container'>
+              <span className='create_post_text'>Create a post</span>
               <br />
-            <input type='text' className='post_text_box' placeholder='Text (optional)' />
+              <select className='choose_community_dropdown'>
+                <option>Choose a community</option>
+                <option>Prolly Won't be a Select Element</option>
+                <option>Prolly Won't be a Select Element</option>
+                <option>Prolly Won't be a Select Element</option>
+                <option>Prolly Won't be a Select Element</option>
+              </select>
+              <br />
 
-            <div className='selections_post_container'>
-              <div className='mark_as_selections'>
-                <div id='oc'>+ OC</div>
-                <div id='spoiler'>+ SPOILER</div>
-                <div id='nsfw'>+ NSFW</div>
+            <div className='post_box'>
+
+              <div className='post_options'>
+                <div id='post'>Post</div>
+                <div id='img_vid'>Image & Video</div>
+                <div id='link'>Link</div>
               </div>
-              <div className='draft_post'>
-                <input type='button' id='draft' value='SAVE DRAFT' />
-                <input type='button' id='submit_post' value='POST' />
+
+              <input type='text' className='title_input' placeholder='Title' />
+                <br />
+              <input type='text' className='post_text_box' placeholder='Text (optional)' />
+
+              <div className='selections_post_container'>
+                <div className='mark_as_selections'>
+                  <div id='oc'>+ OC</div>
+                  <div id='spoiler'>+ SPOILER</div>
+                  <div id='nsfw'>+ NSFW</div>
+                </div>
+                <div className='draft_post'>
+                  <input type='button' id='draft' value='SAVE DRAFT' />
+                  <input type='button' id='submit_post' value='POST' />
+                </div>
               </div>
+            <div className='post_notif'></div>
             </div>
-          <div className='post_notif'></div>
-          </div>
-          </div>
-          <div className='rules_policy_container'></div>
+            </div>
+        </div>
+            <div className='rules_policy_container'></div>
       </>
     )
   }
