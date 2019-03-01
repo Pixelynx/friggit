136a882fcbe8bc34fd6b16742532d78ef6e9c6dc
pixelynx <tjstevens1992@gmail.com>
2019-03-01 14:42:44 -0500

    working on posts reducers

--- frontend/src/reducers/postReducer.js
+++ frontend/src/reducers/postReducer.js
@@ -7,22 +7,61 @@ export const postsAreLoading = (state = false, action) => {
     break;
     default: return state;
   }
-}
+};
 
 export const addPost = (state = [], action) => {
     let newState = state;
-    let postIndex;
 
     switch (action.type) {
       case ADD_POST:
       let idGen = newState.posts[newState.posts.length - 1].id + 1;
       newState.posts.push({
         id: idGen,
-        title: action.payload,
-        body: action.payload
+        title: action.postInput,
+        body: action.postInput
       });
       break;
       default: return state;
     }
     return newState;
-}
+};
+
+export const editPost = (state = posts, action) => {
+  let posts = this.displayPosts.posts;
+  let newState = state;
+  let postIndex;
+
+  switch (action.type) {
+    case EDIT_POST:
+    postIndex = newState.displayPosts.posts.findIndex(post => {
+    return post.id === action.editPost;
+  });
+    newState.posts = newState.posts.slice(0, postIndex).concat(newState.posts.slice(postIndex + 1))
+    break;
+    default: return state;
+  }
+};
+
+export const deletePost = (state = posts, action) => {
+  let posts = this.displayPosts.posts;
+  let newState = state;
+  let postIndex;
+  switch (action.type) {
+    case DELETE_POST:
+    postIndex = newState.posts.findIndex(post => {
+      return post.id === action.deletePost;
+    });
+    newState.posts = newState.posts.slice(0, postIndex).concat(newState.posts.slice(postIndex + 1))
+    break;
+    default: return state;
+  }
+};
+
+export const displayPosts = (state = false, action) => {
+  switch (action.type) {
+    case DISPLAY_POSTS:
+    return action.displayPosts;
+    break;
+    default: return state;
+  }
+};
