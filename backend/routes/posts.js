b33a51dd13c6b62f6d1137c5547567cf1a986148
pixelynx <tjstevens1992@gmail.com>
2019-03-04 22:37:57 -0500

    changes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const queries = require('../database/queries/postQuery.js');
 
-router.get('/submit', queries.getAllPosts);
+router.get('/', queries.getAllPosts);
 router.post('/submit', queries.createNewPost);
 
 module.exports = router;
