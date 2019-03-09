082a08d894532e287c4c08def264ce95c2d7ff7b
pixelynx <tjstevens1992@gmail.com>
2019-03-09 02:36:27 -0500

    posts posting from backend

--- frontend/src/components/posts/posts.jsx
+++ frontend/src/components/posts/posts.jsx
@@ -12,37 +12,35 @@ class Posts extends Component {
   }
 
   componentDidMount = () => {
-    this.makeArr()
-  }
-
-  makeArr = () => {
-    axios.get('/posts')
-    .then(res => {
-      this.setState({ posts: res.data.posts })
-    })
+    this.fetchPosts()
   }
 
   fetchPosts = () => {
     axios.get('/posts')
     .then(res => {
-      this.setState({ posts: res.data })
+      this.setState({ posts: res.data.posts })
     })
-    .catch(err => console.log(err))
-  }
+
+   }
 
 
   render() {
+    const { posts } = this.state;
     console.log(this.state)
-
+    let idGen;
 
     return(
       <>
-        <DisplayPosts/>
+      <div>
+        {posts.map((post) => {
+          return <> <div key={post.id} className='post'>{post.body}</div> </>
+        })
+      }
+      </div>
         </>
     )
   }
 }
-
 
 
 
