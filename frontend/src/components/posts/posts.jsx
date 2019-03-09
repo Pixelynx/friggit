8ab3ff12c69623ad567b172e11a497d4ef031cb4
pixelynx <tjstevens1992@gmail.com>
2019-03-08 22:58:14 -0500

    working on posting to page

--- frontend/src/components/posts/posts.jsx
+++ frontend/src/components/posts/posts.jsx
@@ -3,23 +3,45 @@ import axios from 'axios';
 import PostsForm from './postForm.jsx';
 import * as util from '../../utils/apiCalls.js';
 
-
-import '../../css/posts/createPost.css';
+import '../../css/posts/allPosts.css';
 
 class Posts extends Component {
+  state = {
+    posts: []
+  }
 
   componentDidMount = () => {
     util.getAllPosts()
     .then((res) => {
-      console.log(res)
+      let posts = res.data.posts
+      posts.map(post => {
+      this.state.posts.push(post)
+      this.displayPosts()
+    })
     })
   }
 
+  displayPosts = () => {
+    let allPosts = this.state.posts.map(post => {
+      return (<div className='post'>{post.title}</div>)
+    })
+    if(!this.state.posts) {
+      return(
+        <div className='no_posts'>No posts yet.</div>
+      )
+    } else {
+      return allPosts
+    }
+  }
+
+
+
   render() {
+    console.log(this.state)
+
 
     return(
       <>
-
         </>
     )
   }
